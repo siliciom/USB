ec965a16f0e279476b28f9f495808a31613eb2ad
shruti_patel <shruti_p@siliciom.com>
2026-05-21 16:53:07 +0530

    usb2p0 from docs  directory

--- USB2p0/docs/USB2p0/agents/host_controller_agent/USB2p0_HOST_controller_sequencer.sv
+++
@@ -1,9 +0,0 @@
-class USB2p0_HOST_controller_sequencer extends uvm_sequencer #(USB2p0_sequence_item);
-  
-  `uvm_component_utils(USB2p0_HOST_controller_sequencer)
-  
-  function new(string name="USB2p0_HOST_controller_sequencer",uvm_component parent);
-    super.new(name,parent);
-  endfunction
-  
-endclass
