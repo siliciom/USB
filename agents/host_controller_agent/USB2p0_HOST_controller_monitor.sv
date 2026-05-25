39b026d6c8ac08383908bad894f2c519661166ed
Chandan <chandan_b@siliciom.com>
2026-05-25 20:13:45 +0530

    updated monitor sb,env connection

--- agents/host_controller_agent/USB2p0_HOST_controller_monitor.sv
+++ agents/host_controller_agent/USB2p0_HOST_controller_monitor.sv
@@ -3,10 +3,12 @@ class USB2p0_HOST_controller_monitor extends uvm_monitor;
    `uvm_component_utils(USB2p0_HOST_controller_monitor)
 
 
-   virtual USB2p0_host_utmi_interface         utmi_interface_tx;
-   virtual USB2p0_host_utmi_interface         utmi_interface_rx;
-     // uvm_analysis_port #(USB2p0_sequence_item) host_tx_mon_ap;
-   //   uvm_analysis_port #(USB2p0_sequence_item) host_rx_mon_ap;
+      virtual USB2p0_host_utmi_interface         utmi_interface_tx;
+      virtual USB2p0_host_utmi_interface         utmi_interface_rx;
+
+      uvm_analysis_port #(USB2p0_sequence_item) host_tx_mon_ap;
+      uvm_analysis_port #(USB2p0_sequence_item) host_rx_mon_ap;
+
       USB2p0_sequence_item  usb_host_tx_seq_item;
       USB2p0_sequence_item  usb_host_rx_seq_item;
       USB2p0_sequence_item usb_host_seq_item;
@@ -27,8 +29,8 @@ class USB2p0_HOST_controller_monitor extends uvm_monitor;
  
       function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        //host_tx_mon_ap = new("host_tx_mon_ap", this);
-       // host_rx_mon_ap = new("host_rx_mon_ap", this);
+         host_tx_mon_ap = new("host_tx_mon_ap", this);
+         host_rx_mon_ap = new("host_rx_mon_ap", this);
          usb_host_seq_item = USB2p0_sequence_item::type_id::create("usb_host_seq_item");
          usb_event_pool = uvm_event_pool::get_global_pool();
          ack_done_ev = usb_event_pool.get("ACK_DONE_EVENT");
@@ -57,7 +59,8 @@ task collect_utmi_tx_data();
    bit [15:0] first_byte_16;
 forever begin
    `uvm_info("HOST_MONITOR","ENTERED_INTO_COLLECT_UTMI_TX_SIGNALS",UVM_LOW)
-   usb_host_seq_item = USB2p0_sequence_item::type_id::create("usb_host_seq_item");
+    usb_host_seq_item = USB2p0_sequence_item::type_id::create("usb_host_seq_item");
+    tx_queue.delete();
    wait (utmi_interface_tx.utmi_txvalid && utmi_interface_tx.utmi_txready);
       @(negedge utmi_interface_tx.cb_utmi_host_controller_monitor);
       //@(negedge utmi_interface_tx.utmi_clk);
@@ -93,6 +96,7 @@ forever begin
       `uvm_info("HOST_MONITOR","WHILE_LOOP_EXIT_IN_HOST_MONITOR",UVM_LOW)
        `uvm_info("HOST_MONITOR","COLLECTED_UTMI_TX_SIGNALS",UVM_LOW)
        usb_host_seq_item.print();
+       host_tx_mon_ap.write(usb_host_seq_item);
      end
 endtask
 
@@ -105,7 +109,7 @@ task collect_from_utmi_rx();
 
 forever begin
    usb_host_rx_seq_item = USB2p0_sequence_item::type_id::create("usb_host_rx_seq_item");
-
+   tx_queue.delete();
    wait (utmi_interface_rx.utmi_rxvalid || utmi_interface_rx.utmi_rxvalidh);
      @(negedge utmi_interface_rx.cb_utmi_host_controller_monitor);
      usb_host_rx_seq_item.rx_valid       = utmi_interface_rx.utmi_rxvalid;
@@ -135,9 +139,20 @@ forever begin
                usb_host_rx_seq_item.rx_data.push_back(collect_data8);
               `uvm_info("HOST_MONITOR",$sformatf("8BIT_DATA_ON_RX = %p", usb_host_rx_seq_item.rx_data),UVM_LOW)
            end
+           //`uvm_info("HOST_MONITOR","HOST_RX_PACKET_COMPLETE",UVM_LOW)
+            //usb_host_rx_seq_item.print();
+            //host_rx_mon_ap.write(usb_host_rx_seq_item);
           end
            `uvm_info("HOST_MONITOR","HOST_RX_PACKET_COMPLETE",UVM_LOW)
             usb_host_rx_seq_item.print();
+	    if(usb_host_rx_seq_item.rx_data.size() > 0)
+            usb_host_rx_seq_item.rx_valid = 1'b1;
+            if(usb_host_rx_seq_item.word_if &&
+            usb_host_rx_seq_item.rx_data.size() > 0) begin
+	    usb_host_rx_seq_item.rx_valid  = 1'b1;
+            usb_host_rx_seq_item.rx_validh = 1'b1;
+            end
+            host_rx_mon_ap.write(usb_host_rx_seq_item);
           // if (usb_host_rx_seq_item.rx_data.size() == 1 &&  usb_host_rx_seq_item.rx_data[0] == 8'h2D) begin
           //        `uvm_info("DEVICE_MONITOR","ACK_RECEIVED_FROM_HOST_DATA_STAGE_COMPLETE",UVM_LOW)
           // end
--- env/USB2p0_scoreboard.sv
+++ env/USB2p0_scoreboard.sv
@@ -1,17 +1,144 @@
+//=====================scoreboard=======================================
+`uvm_analysis_imp_decl(_host_tx)
+`uvm_analysis_imp_decl(_host_rx)
+`uvm_analysis_imp_decl(_device_tx)
+`uvm_analysis_imp_decl(_device_rx)
+
 class usb2p0_scoreboard extends uvm_scoreboard;
-  
-   `uvm_component_utils(usb2p0_scoreboard)
-  
-    USB2p0_sequence_item                                        usb_seq_item;
+
+  `uvm_component_utils(usb2p0_scoreboard)
+
+  uvm_analysis_imp_host_tx   #(USB2p0_sequence_item,usb2p0_scoreboard) host_tx_imp;
+  uvm_analysis_imp_host_rx   #(USB2p0_sequence_item,usb2p0_scoreboard) host_rx_imp;
+
+  uvm_analysis_imp_device_tx #(USB2p0_sequence_item,usb2p0_scoreboard) device_tx_imp;
+  uvm_analysis_imp_device_rx #(USB2p0_sequence_item,usb2p0_scoreboard) device_rx_imp;
+
+  bit [DATA_WIDTH-1:0] host_tx_q[$][$];
+  bit [DATA_WIDTH-1:0] host_rx_q[$][$];
+
+  bit [DATA_WIDTH-1:0] device_tx_q[$][$];
+  bit [DATA_WIDTH-1:0] device_rx_q[$][$];
 
   function new(string name="usb2p0_scoreboard",uvm_component parent);
-    super.new(name,parent);  
+    super.new(name,parent);
   endfunction
-  
+
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
+    host_tx_imp   = new("host_tx_imp",this);
+    host_rx_imp   = new("host_rx_imp",this);
+    device_tx_imp = new("device_tx_imp",this);
+    device_rx_imp = new("device_rx_imp",this);
   endfunction
-  
+
+
+  //=================HOST TX================================
+  function void write_host_tx(USB2p0_sequence_item pkt);
+  `uvm_info("HOST_TX_SB",$sformatf("HOST_TX_DATA"),UVM_LOW)
+   pkt.print();
+      if(pkt.tx_valid || pkt.tx_validh) begin
+         if(pkt.tx_valid)begin
+           host_tx_q.push_back(pkt.utmi_txdata);
+           `uvm_info("HOST_TX_SB",$sformatf("8BIT_HOST_TX DATA=%p 8_BIT_QUEUE=%P",pkt.utmi_txdata,host_tx_q),UVM_LOW)
+         end
+        else if(pkt.tx_validh) begin
+          host_tx_q.push_back(pkt.utmi_txdata);
+          `uvm_info("HOST_TX_SB",$sformatf("16BIT_HOST_TX DATA=%p 16_BIT_QUEUE=%P VALIDH=%0b ",pkt.utmi_txdata,host_tx_q,pkt.tx_validh),UVM_LOW)
+        end
+      end
+    compare_host_to_device();
+  endfunction
+
+  //=================DEVICE RX==============================
+  function void write_device_rx(USB2p0_sequence_item pkt);
+   `uvm_info("DEVICE_RX_SB",$sformatf("DEVICE_RX_DATA"),UVM_LOW)
+    pkt.print();
+      if(pkt.rx_valid || pkt.rx_validh) begin
+         if(pkt.rx_valid)begin
+            device_rx_q.push_back(pkt.rx_data);
+           `uvm_info("DEVICE_RX_SB",$sformatf("8BIT_DEVICE_RX DATA=%p 8_BIT_QUEUE=%p",pkt.rx_data,device_rx_q),UVM_LOW)
+         end
+        else if(pkt.rx_validh) begin
+           device_rx_q.push_back(pkt.rx_data);
+          `uvm_info("DEVICE_RX_SB",$sformatf("16BIT_DEVICE_RX DATA=%p 16_BIT_QUEUE=%p",pkt.rx_data,device_rx_q),UVM_LOW)
+        end
+      end
+    compare_host_to_device();
+  endfunction
+
+  //===================DEVICE TX============================
+  function void write_device_tx(USB2p0_sequence_item pkt);
+  `uvm_info("DEVICE_TX_SB",$sformatf("DEVICE_TX_DATA"),UVM_LOW)
+   pkt.print();
+      if(pkt.tx_valid || pkt.tx_validh) begin
+         if(pkt.tx_valid)begin
+           device_tx_q.push_back(pkt.utmi_txdata);
+           `uvm_info("DEVICE_TX_SB",$sformatf("8BIT_DEVICE_TX DATA=%p 8_BIT_QUEUE=%P",pkt.utmi_txdata,device_tx_q),UVM_LOW)
+         end
+         else if(pkt.tx_validh) begin
+           device_tx_q.push_back(pkt.utmi_txdata);
+          `uvm_info("DEVICE_TX_SB",$sformatf("16BIT_DEVICE_TX DATA=%p 16_BIT_QUEUE=%P VALIDH=%0b ",pkt.utmi_txdata,device_tx_q,pkt.tx_validh),UVM_LOW)
+         end
+      end
+    compare_device_to_host();
+  endfunction
+
+
+  //=======================HOST RX==========================
+  function void write_host_rx(USB2p0_sequence_item pkt);
+  `uvm_info("HOST_RX_SB",$sformatf("HOST_RX_DATA"),UVM_LOW)
+   pkt.print();
+       if(pkt.rx_valid || pkt.rx_validh) begin
+         if(pkt.rx_valid)begin
+           host_rx_q.push_back(pkt.rx_data);
+          `uvm_info("HOST_RX_SB",$sformatf("8BIT_HOST_RX DATA=%p 8_BIT_QUEUE=%p",pkt.rx_data,host_rx_q),UVM_LOW)
+         end
+         else if(pkt.rx_validh) begin
+           host_rx_q.push_back(pkt.rx_data);
+          `uvm_info("HOST_RX_SB",$sformatf("16BIT_HOST_RX DATA=%p 16_BIT_QUEUE=%P VALIDH=%0b ",pkt.utmi_txdata,host_rx_q,pkt.tx_validh),UVM_LOW)
+          end
+      end
+     compare_device_to_host();
+  endfunction
+
+  //=============HOST TO DEVICE COMPARE====================
+  function void compare_host_to_device();
+    bit [DATA_WIDTH-1:0] exp_data[$];
+    bit [DATA_WIDTH-1:0] act_data[$];
+    if((host_tx_q.size() > 0) &&
+       (device_rx_q.size() > 0)) begin
+      act_data = host_tx_q.pop_front();
+      exp_data = device_rx_q.pop_front();
+      if(exp_data == act_data) begin
+        `uvm_info("HOST_DEVICE_COMPARE",$sformatf("SB_DATA_MATCHED EXP=%p ACT=%p",exp_data,act_data),UVM_LOW)
+      end
+      else begin
+        `uvm_error("HOST_DEVICE_COMPARE",$sformatf("SB_DATA_MISMATCH EXP=%p ACT=%p",exp_data,act_data))
+      end
+    end
+  endfunction
+
+
+  //===============DEVICE TO HOST COMPARE====================
+  function void compare_device_to_host();
+    bit [DATA_WIDTH-1:0] exp_data[$];
+    bit [DATA_WIDTH-1:0] act_data[$];
+    //device_tx_q.delete();
+    if((device_tx_q.size() > 0) &&
+       (host_rx_q.size() > 0)) begin
+      act_data = device_tx_q.pop_front();
+      //`uvm_info("DEVICE_HOST_COMPARE",$sformatf("DEVICE_HOST_ACT=%p",act_data),UVM_LOW)
+      exp_data = host_rx_q.pop_front();
+      //`uvm_info("DEVICE_HOST_COMPARE",$sformatf("DEVICE_HOST_EXP=%p",exp_data),UVM_LOW)
+      if(exp_data == act_data) begin
+      `uvm_info("DEVICE_HOST_COMPARE",$sformatf("SB_DATA_MATCHED EXP=%p ACT=%p",exp_data,act_data),UVM_LOW)
+      end
+      else begin
+      `uvm_error("DEVICE_HOST_COMPARE",$sformatf("SB_DATA_MISMATCH EXP=%p ACT=%p",exp_data,act_data))
+      end
+    end
+  endfunction
 
 endclass
 
